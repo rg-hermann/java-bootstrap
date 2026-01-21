e66b59d5c9a46f18fda4cfb24704db9a6f784c77
Rodrigo Garcez Hermann <rodrigohermann18@outlook.com>
2026-01-20 21:54:53 -0300

    chore: update Spring Boot version and adjust test configuration

--- src/test/java/com/example/adapter/rest/HealthControllerTest.java
+++ src/test/java/com/example/adapter/rest/HealthControllerTest.java
@@ -3,16 +3,16 @@ package com.example.adapter.rest;
 import com.example.config.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@WebMvcTest(HealthController.class)
-@ContextConfiguration(classes = Application.class)
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
 class HealthControllerTest {
 
     @Autowired
